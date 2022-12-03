ee30a76030a324552cbc37b5535881f1c2cf1f6d
Alison Chaiken <alison@she-devel.com>
2022-12-03 09:04:54 -0800

    Pass tested matrix as a function parameter

--- matrix-determinant_testsuite.cc
+++ matrix-determinant_testsuite.cc
@@ -4,9 +4,12 @@
 
 #include "matrix-determinant.c"
 
+const double test_matrix[SIZE][SIZE] = {
+    {0.0, 2.0, 2.0}, {6.0, 4.0, 10.0}, {6.0, 14.0, 8.0}};
+
 TEST(SimpleMatrixTest, FindIndex) {
-  EXPECT_EQ(1U, find_row_index((&test_matrix[0][0]) + 3));
-  EXPECT_EQ(0U, find_column_index((&test_matrix[0][0]) + 3));
+  EXPECT_EQ(1U, find_row_index((&test_matrix[0][0]) + 3, test_matrix));
+  EXPECT_EQ(0U, find_column_index((&test_matrix[0][0]) + 3, test_matrix));
 }
 
 TEST(SimpleMatrixTest, Equality) {
